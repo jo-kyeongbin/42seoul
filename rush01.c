3aaf5a09d289014041e1c1688309acde6bdac2e7
JKB <jkb5570@gmail.com>
2020-08-02 01:16:38 +0900

    new

--- rush01.c
+++ rush01.c
@@ -3,87 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   rush01.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kjo <kjo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: kyeongbinjo <kyeongbinjo@student..fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/01 16:21:58 by kjo               #+#    #+#             */
-/*   Updated: 2020/08/01 19:49:48 by kjo              ###   ########.fr       */
+/*   Updated: 2020/08/02 01:14:53 by kyeongbinjo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#include "ft_putchar.c"
+
 void	rush(int, int);
 void	print_slash(void);
 void	print_star(void);
 void	print_back_s(void);
 void	print_jump(void);
+void	ft_putchar(char);
 
-int main(void){
-	printf("1,5\n");
-	rush(1,5);	
-	printf("1,1\n");
-	rush(1,1);
-	printf("4,4\n");
-	rush(4,4);
-	printf("5,1\n");
-	rush(5,1);
-	return 0;
-}
-
-void rush (int a, int b){
-	int row = 0;
-	int col = 0;
-	char next = '\n';
+void	rush (int a, int b){
+	int row;
+	row = 0;
+	int col;
+	col = 0;
+	char next;
+	next = '\n';
 	
 	while(!(row == b && col == a)){
-	if((row == 0 && col == 0) || (row != 0 && row == b-1 && col == a-1)){
-		print_slash();
-		//printf("slash:%d, %d, %d, %d\n",row,col,a,b);
-	}
+		if((row == 0 && col == 0) || (row != 0 && row == b-1 && col == a-1)){
+			print_slash();
+		}
 	
-	else if(((row == 0 || row == b-1) && (col >= 1 && col <= a-2)) ||
-		((row >= 1 && row <= b-2) && (col == 0 || col == a-1))){
-		print_star();
-		//printf("star:%d, %d, %d, %d\n",row,col,a,b);
-	}
+		else if(((row == 0 || row == b-1) && (col >= 1 && col <= a-2)) ||
+			((row >= 1 && row <= b-2) && (col == 0 || col == a-1))){
+			print_star();
+		}
 
-	else if((row == 0 && col == a-1) || (row == b-1 && col == 0)){
-		print_back_s();
-		//printf("back:%d, %d, %d, %d\n",row,col,a,b);
-	}
+		else if((row == 0 && col == a-1) || (row == b-1 && col == 0)){
+			print_back_s();
+		}
 
-	else if((row >= 1 && row < b-1) && (col >= 1 && col < a-1)){
-		print_jump();
-		//printf("jump:%d, %d, %d, %d\n",row,col,a,b);
-	}
+		else if((row >= 1 && row < b-1) && (col >= 1 && col < a-1)){
+			print_jump();
+		}
 
-	else if(col==a){
-		write(1,&next,1);
-		col = 0;
-		row++;
-		col--;
-	}
+		else if(col==a){
+			write(1,&next,1);
+			col = 0;
+			row++;
+			col--;
+		}
 	col++;
 	}
 }
 
-void print_slash(void){
-	char s = '/';
-	write(1,&s,1);
+void	print_slash(void){
+	char s;
+	s = '/';
+	ft_putchar(s);
 }
 
 void print_star(void){
-	char s = '*';
-	write(1,&s,1);
+	char s;
+	s = '*';
+	ft_putchar(s);
 }
 
 void print_back_s(void){
-	char s = '\\';
-	write(1,&s,1);
+	char s;
+	s = '\\';
+	ft_putchar(s);
 }
 
 void print_jump(void){
-	char s = ' ';
-	write(1,&s,1);
+	char s;
+	s = ' ';
+	ft_putchar(s);
 }
 
