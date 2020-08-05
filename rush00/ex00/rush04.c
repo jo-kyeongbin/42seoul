b001c5552d05a03348e31a2cb2ba261e409250f4
Kyeongbin Jo <kjo@c6r3s7.42seoul.kr>
2020-08-05 09:37:10 +0900

    m

---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjo <kjo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/02 14:26:07 by kjo               #+#    #+#             */
+/*   Updated: 2020/08/02 15:11:02 by kjo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	print_square(int a, int b, int col, int row)
+{
+	if (row == 0)
+	{
+		if (col == 0)
+			ft_putchar('A');
+		else if (col == a - 1)
+			ft_putchar('C');
+		else
+			ft_putchar('B');
+	}
+	else if (row == b - 1)
+	{
+		if (col == 0)
+			ft_putchar('C');
+		else if (col == a - 1)
+			ft_putchar('A');
+		else
+			ft_putchar('B');
+	}
+	else
+	{
+		if (col == 0 || col == a - 1)
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+	}
+}
+
+void	rush(int a, int b)
+{
+	int		row;
+	int		col;
+	char	next;
+
+	row = 0;
+	col = 0;
+	next = '\n';
+	while (row < b)
+	{
+		col = 0;
+		while (col < a)
+		{
+			print_square(a, b, col, row);
+			col++;
+		}
+		ft_putchar(next);
+		row++;
+	}
+}
