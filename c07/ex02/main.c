fe71b8ad9a4fcf0feb90da50f588a20a6cb44bc3
Kyeongbin Jo <kjo@c1r4s6.42seoul.kr>
2020-08-18 16:50:03 +0900

    mod

--- c07/ex02/main.c
+++ c07/ex02/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
  int ft_ultimate_range(int **range, int min, int max);
 
  int main()
  {
 	 int **temp;
+	 temp = (int **)malloc(sizeof(int*)*4);
 	 int b = ft_ultimate_range(temp, -5, -1);
 	 printf("return : %d\n", b);
 	 for (int i = 0; i < 4; i++)
